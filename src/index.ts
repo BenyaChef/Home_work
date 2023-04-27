1625d40d954ad46066129d7997b1fc896cd14f3e
Maxim <benyachef@gmail.com>
2023-04-27 22:40:16 +0300

    app.put test

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,75 @@
-import express, {Request, Response}  from 'express'
-const app = express()
-const port = 3000
+import express, {Request, Response} from 'express'
+import bodyParser from 'body-parser'
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+const app = express()
+const port = process.env.PORT || 3000
+
+const createdDate = new Date()
+
+enum resolution {
+    P144 = 'P144'
+}
+
+const videoDB = [{
+    id: 1,
+    title: 'Through hardship to the stars',
+    author: 'Richard Viktorov',
+    canBeDownloaded: true,
+    minAgeRestriction: 14,
+    createdAt: '1981-04-23T12:00:53.661Z',
+    publicationDate: '1981-04-24T12:00:53.661Z',
+    availableResolution: [
+        'P720'
+    ]
+}]
+
+const parserMiddleware = bodyParser();
+app.use(parserMiddleware)
+
+app.delete('/testing/all-data', (req: Request, res: Response) => {
+    videoDB.splice(0);
+    res.send(204)
 })
+app.get('/videos', (req: Request, res: Response) => {
+    res.send(videoDB);
+})
+app.get('/videos/:id', (req: Request, res: Response) => {
+    const video = videoDB.find(v => v.id === +req.params.id)
+    if (video) {
+        res.send(video)
+    } else {
+        res.send(404)
+    }
+
+})
+app.post('/videos', (req: Request, res: Response) => {
+    const newVideo = {
+        id: +(new Date()),
+        title: req.body.title,
+        author: req.body.author,
+        canBeDownloaded: false,
+        minAgeRestriction: 12,
+        createdAt: createdDate.toISOString(),
+        publicationDate: new Date(createdDate.setDate(createdDate.getDate() + 1)).toISOString(),
+        availableResolution: req.body.resolution
+
+    }
+    videoDB.push(newVideo)
+    res.status(201).send(newVideo);
+})
+app.put('/videos/:id', (req: Request, res: Response) => {
+
+})
+app.delete('/videos/:id', (req: Request, res: Response) => {
+    for(let i = 0; i < videoDB.length; i++) {
+        if(videoDB[i].id === +req.params.id) {
+            videoDB.splice(i, 1)
+            res.send(204)
+            return
+        }
+    }
+ })
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
