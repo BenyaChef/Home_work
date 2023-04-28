0c9c76a166bff4da00ae53336d35f8e87e50ac9d
Maxim <benyachef@gmail.com>
2023-04-28 16:29:32 +0300

    first commit

--- dist/index.js
+++ dist/index.js
@@ -8,10 +8,17 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 const createdDate = new Date();
-var resolution;
-(function (resolution) {
-    resolution["P144"] = "P144";
-})(resolution || (resolution = {}));
+var resolutionDB;
+(function (resolutionDB) {
+    resolutionDB["P144"] = "P144";
+    resolutionDB["P240"] = "P240";
+    resolutionDB["P360"] = "P360";
+    resolutionDB["P480"] = "P480";
+    resolutionDB["P720"] = "P720";
+    resolutionDB["P1080"] = "P1080";
+    resolutionDB["P1440"] = "P1440";
+    resolutionDB["P2160"] = "P2160";
+})(resolutionDB || (resolutionDB = {}));
 const videoDB = [{
         id: 1,
         title: 'Through hardship to the stars',
@@ -20,9 +27,7 @@ const videoDB = [{
         minAgeRestriction: 14,
         createdAt: '1981-04-23T12:00:53.661Z',
         publicationDate: '1981-04-24T12:00:53.661Z',
-        availableResolution: [
-            'P720'
-        ]
+        availableResolutions: [resolutionDB.P720]
     }];
 const parserMiddleware = (0, body_parser_1.default)();
 app.use(parserMiddleware);
@@ -47,23 +52,32 @@ app.post('/videos', (req, res) => {
         id: +(new Date()),
         title: req.body.title,
         author: req.body.author,
-        canBeDownloaded: false,
-        minAgeRestriction: 12,
+        canBeDownloaded: req.body.canBeDownloaded || false,
+        minAgeRestriction: req.body.minAgeRestriction || null,
         createdAt: createdDate.toISOString(),
         publicationDate: new Date(createdDate.setDate(createdDate.getDate() + 1)).toISOString(),
-        availableResolution: req.body.resolution
+        availableResolutions: req.body.availableResolutions
     };
     videoDB.push(newVideo);
     res.status(201).send(newVideo);
 });
 app.put('/videos/:id', (req, res) => {
+    const video = videoDB.find(v => v.id = +req.params.id);
+    if (video) {
+        video.title = req.body.title;
+        video.author = req.body.author;
+        video.canBeDownloaded = req.body.canBeDownloaded || false;
+        video.minAgeRestriction = req.body.minAgeRestriction || null;
+        video.publicationDate = req.body.publicationDate;
+        video.availableResolutions = req.body.availableResolutions;
+        res.send(204);
+    }
 });
 app.delete('/videos/:id', (req, res) => {
     for (let i = 0; i < videoDB.length; i++) {
         if (videoDB[i].id === +req.params.id) {
             videoDB.splice(i, 1);
             res.send(204);
-            return;
         }
     }
 });
--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,17 @@ const port = process.env.PORT || 3000
 
 const createdDate = new Date()
 
-enum resolution {
-    P144 = 'P144'
+enum resolutionDB {
+    P144 = 'P144',
+    P240 = 'P240',
+    P360 = 'P360',
+    P480 = 'P480',
+    P720 = 'P720',
+    P1080 = 'P1080',
+    P1440 = 'P1440',
+    P2160 = 'P2160'
 }
+
 
 const videoDB = [{
     id: 1,
@@ -18,9 +26,7 @@ const videoDB = [{
     minAgeRestriction: 14,
     createdAt: '1981-04-23T12:00:53.661Z',
     publicationDate: '1981-04-24T12:00:53.661Z',
-    availableResolution: [
-        'P720'
-    ]
+    availableResolutions: [resolutionDB.P720]
 }]
 
 const parserMiddleware = bodyParser();
@@ -47,25 +53,33 @@ app.post('/videos', (req: Request, res: Response) => {
         id: +(new Date()),
         title: req.body.title,
         author: req.body.author,
-        canBeDownloaded: false,
-        minAgeRestriction: 12,
+        canBeDownloaded: req.body.canBeDownloaded || false,
+        minAgeRestriction: req.body.minAgeRestriction || null,
         createdAt: createdDate.toISOString(),
         publicationDate: new Date(createdDate.setDate(createdDate.getDate() + 1)).toISOString(),
-        availableResolution: req.body.resolution
+        availableResolutions: req.body.availableResolutions
 
     }
     videoDB.push(newVideo)
     res.status(201).send(newVideo);
 })
 app.put('/videos/:id', (req: Request, res: Response) => {
-
+    const video = videoDB.find(v => v.id = +req.params.id)
+    if(video) {
+            video.title = req.body.title
+            video.author = req.body.author
+            video.canBeDownloaded = req.body.canBeDownloaded || false
+            video.minAgeRestriction = req.body.minAgeRestriction || null
+            video.publicationDate = req.body.publicationDate
+            video.availableResolutions = req.body.availableResolutions
+            res.send(204)
+    }
 })
 app.delete('/videos/:id', (req: Request, res: Response) => {
     for(let i = 0; i < videoDB.length; i++) {
         if(videoDB[i].id === +req.params.id) {
             videoDB.splice(i, 1)
             res.send(204)
-            return
         }
     }
  })
